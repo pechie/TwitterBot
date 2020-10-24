f37d5821215de09d7d1bbbe2776d1af350a199c3
Nikolas Pechie <pechie.n@northeastern.edu>
2020-10-24 15:21:32 -0400

    Working

--- twitter_bot.js
+++ twitter_bot.js
@@ -3,10 +3,10 @@ const twit = require('twit');
 const T = new twit(config);
 
 // Possible responses
-const messages = ["Bold and brash", 
-                  "More like, belongs in the trash!"];
+const messages = ['Bold and brash', 
+                  'More like, belongs in the trash!'];
 // @ of the bot
-const user = "@BoldAndBrashBot";
+const user = '@BoldAndBrashBot';
 
 let stream = T.stream('statuses/filter', { track: [user] });
 stream.on('tweet', tweetEvent);
@@ -15,14 +15,24 @@ function tweetEvent(tweet) {
     // Get ID of tweet to reply to
     let nameID  = tweet.id_str;
 
+    // Favorite the tweet to show some common decency
+    T.post('favorites/create', { id: nameID }, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Favorited tweet with id: ' + nameID);
+        }
+    });
+
     // Formatting reply, need to include the @ first
-    let mention = "@" + tweet.user.screen_name + " ";
+    let mention = '@' + tweet.user.screen_name + " ";
     let reply = (Math.random() >= 0.5) ? messages[0] : messages[1];
     let params = {
         status: mention + reply,
         in_reply_to_status_id: nameID
     };
 
+    // Tweeting reply
     T.post('statuses/update', params, function(err, data, response) {
       if (err) {
         console.log(err);
